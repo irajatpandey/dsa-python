a53151e59cbefdcb276d56c8e648e6b13def4844
Rajat Pandey <rajatpandey7800@gmail.com>
2025-06-04 08:38:49 +0530

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/

--- Arrays/Easy/MoveZeroes.py
+++ Arrays/Easy/MoveZeroes.py
@@ -1,7 +1,7 @@
-    def moveZeroes(self, nums):
-        i, j = 0, 0
-        while i < len(nums):
-            if nums[i] != 0:
-               nums[i], nums[j] = nums[j], nums[i]
-               j += 1
-            i += 1 
+def moveZeroes(self, nums):
+    i, j = 0, 0
+    while i < len(nums):
+        if nums[i] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            j += 1
+        i += 1 
